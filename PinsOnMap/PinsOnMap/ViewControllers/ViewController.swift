24e3b46b6aa8e8ca8ed11c14ed5cec75364efe93
Wojciech Charysz <>
2018-04-21 22:08:26 +0200

    Adding UI elements

--- PinsOnMap/PinsOnMap/ViewControllers/ViewController.swift
+++ PinsOnMap/PinsOnMap/ViewControllers/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
