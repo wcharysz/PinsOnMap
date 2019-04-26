017bbd4034b4ff4767a996ccde4cdde69db94396
Wojciech Charysz <>
2019-04-26 12:34:56 +0200

    Added documentation to the ViewModel.

--- PinsOnMap/PinsOnMap/ViewControllers/ViewModel.swift
+++ PinsOnMap/PinsOnMap/ViewControllers/ViewModel.swift
@@ -8,14 +8,19 @@
 
 import Foundation
 
+/**
+ This is a class, which provides content for the ViewController.
+*/
 class ViewModel {
     
+    /// A weak reference to the instance of the ViewController.
     unowned var delegate: ViewController
     
     init(_ viewController: ViewController) {
         delegate = viewController
     }
     
+    /// The start date for the searched Places. It's set to 1990 year.
     lazy var openFrom: Date? = {
         var dateComponenets = DateComponents()
         dateComponenets.calendar = Calendar(identifier: .gregorian)
@@ -31,6 +36,7 @@ class ViewModel {
         return queryLimit / queryOffset
     }
     
+    /// A container for Places models. After it's set the array is filtered out of Places which are open earlier than the date defined by openFrom property. From the rest of the places are created Pin objects and added to the mapPins property.
     var places: [Places] = [] {
         didSet {
             let currentPlaces = places
@@ -56,6 +62,7 @@ class ViewModel {
         }
     }
     
+    /// A container for Pin models. After it's set from the array are removed any nils and on the main DispatchQueue are added Pins and displayed. The next step is to schedule a timer which will remove the pins from the map view according to its lifeSpan.
     var mapPins: [Pin?] = [] {
         didSet {
             let currentPins = mapPins
@@ -76,6 +83,10 @@ class ViewModel {
         }
     }
     
+    /** This method clears out properties for places and mapPins and downloads new places from the musicbrainz.org.
+     
+     - Parameter query: The text which should contain place's name.
+    */
     func downloadPlaces(forQuery query: String) {
         let networking = Networking()
         
